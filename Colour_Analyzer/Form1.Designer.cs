50133d970a114e89230383f09f29136b348dafb3
Alluysios Arriba <30941996+Alluysios@users.noreply.github.com>
2021-11-12 20:40:31 -0800

    Colour_Analyzer
    Analyze image file and return  % of rgb

---
+++ Colour_Analyzer/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace Colour_Analyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputInfo = new System.Windows.Forms.TextBox();
+            this.goBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputInfo
+            // 
+            this.outputInfo.Location = new System.Drawing.Point(12, 12);
+            this.outputInfo.Multiline = true;
+            this.outputInfo.Name = "outputInfo";
+            this.outputInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputInfo.Size = new System.Drawing.Size(660, 197);
+            this.outputInfo.TabIndex = 0;
+            // 
+            // goBtn
+            // 
+            this.goBtn.BackColor = System.Drawing.Color.RoyalBlue;
+            this.goBtn.ForeColor = System.Drawing.Color.MintCream;
+            this.goBtn.Location = new System.Drawing.Point(575, 215);
+            this.goBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.goBtn.Name = "goBtn";
+            this.goBtn.Size = new System.Drawing.Size(97, 34);
+            this.goBtn.TabIndex = 1;
+            this.goBtn.Text = "Go!";
+            this.goBtn.UseVisualStyleBackColor = false;
+            this.goBtn.Click += new System.EventHandler(this.goBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 261);
+            this.Controls.Add(this.goBtn);
+            this.Controls.Add(this.outputInfo);
+            this.MaximumSize = new System.Drawing.Size(700, 300);
+            this.Name = "Form1";
+            this.Text = "Image Info Scanner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox outputInfo;
+        private System.Windows.Forms.Button goBtn;
+    }
+}
+
---
+++ Colour_Analyzer/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Colour_Analyzer
+{
+    public partial class Form1 : Form
+    {
+        int fileCount;       // number of threads running
+        int mThreadFinished;    // number of threads finished executing
+        public delegate void delUpdateOutput(string res);   // Update the textbox output 
+        Thread th;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void goBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();  // create open file dialog instance
+
+            ofd.Filter = "Image File|*.JPG|*.PNG|"; // filter image that is allowed
+            ofd.Multiselect = true; // enable multiselect file
+
+            // run code when pressed okay
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                outputInfo.Text += $"Starting...{System.Environment.NewLine}";  // display output that the thread is "Starting..."
+                fileCount = ofd.FileNames.Length;    // initialize the number of files selected
+                mThreadFinished = 0;    // initialize to 0, the number of thread that is finish
+
+                // loop each file that is selected
+                foreach (String file in ofd.FileNames)
+                {
+                    try
+                    {
+                        // create a new parameterized thread instance that analyze the image rgb color
+                        th = new Thread(new ParameterizedThreadStart(AnalyzeImage));
+                        // start the thread with passed parameter file
+                        th.Start(file);
+                    }
+                    // display errors in output
+                    catch (Exception ex)
+                    {
+                        Trace.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+
+        /**
+         * Calculate the image rgb color by percentage
+         * @param   {object}    file    The object file path
+         */
+        private void AnalyzeImage(object file)
+        {
+            string res; // output results
+            Bitmap img = new Bitmap(file.ToString());   // create instance of bitmap of file image
+            Color c;    // image file color
+            double
+             rgbTotal,       // total RGB
+             red = 0,        // total red percentage of image
+             green = 0,      // total green percentage of image
+             blue = 0;       // total blue percentage of image
+
+            // loop over the image by pixels
+            for (int y = 0; y < img.Height; y++)
+                for (int x = 0; x < img.Width; x++)
+                {
+                    c = img.GetPixel(x, y); // get the rgb color of pixel position x,y
+                    red += c.R;     // get red
+                    green += c.G;   // get green
+                    blue += c.B;    // get blue
+                }
+
+            // set the total rgb
+            rgbTotal = (red + green + blue) / 100;
+
+            red /= rgbTotal;    // calculate red percentage
+            green /= rgbTotal;  // calculate green percentage
+            blue /= rgbTotal;   // calculate blue percentage
+                
+            // set the results of image rgb
+            res = $"(R:{red:F1}%, G:{green:F1}%, B:{blue:F1}%) : {file}{System.Environment.NewLine}";
+
+            try
+            {
+                // update the output textbox with the results
+                this.Invoke(new delUpdateOutput(updateFileOutput), res);
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex.Message);
+            }
+        }
+
+        /**
+         *  Update the output textbox text
+         *  @param  {string}     res     The results of the analyzed image.
+         */
+        public void updateFileOutput(string res)
+        {
+            mThreadFinished++;      // thread finished increment by 1
+            outputInfo.Text += res; // set the output textbox text
+
+            // if threadfinish is equal to the selected files means all thread are done, set output text to "Done...".
+            if (mThreadFinished == fileCount)   
+                outputInfo.Text += $"Done...{System.Environment.NewLine}";
+        }
+    }
+}
